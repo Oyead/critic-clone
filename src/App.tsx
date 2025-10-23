0a571f1b3944f767bc7d93fb632ea999cbdd5b71
Eyad <eyadswailam12345@gmail.com>
2025-10-23 20:30:11 +0300

    implement logging in basic logic and add basic navbar elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,49 @@
+import { useState } from "react"
+import { FaRegUserCircle } from "react-icons/fa";
+import GameSections from "./componenets/GameSections";
 function App() {
-
+const [isLoggedIn,setIsLoggedIn] = useState(false) 
   return (
     <>
   <nav className="bg-white shadow-md px-6 py-3 flex items-center justify-between">
       {/*Logo */}
       <div className="flex items-center space-x-2">
         <img
-          src="https://via.placeholder.com/40"
+          src="/src/assets/gamepad.png"
           alt="Logo"
-          className="w-10 h-10 rounded-full"
+          className="w-10 h-10 rounded-full cursor-pointer"
+          title="Critic Clone"
         />
-        <span className="text-xl font-semibold text-gray-800">MyBrand</span>
+       <div className="hidden md:flex space-x-6 ml-8">
+  <a href="#" className="text-gray-700 hover:text-yellow-800 font-medium">Explore</a>
+  <a href="#" className="text-gray-700 hover:text-yellow-800 font-medium">Reviews</a>
+</div>
       </div>
-
       {/* Middle: Search Bar */}
-      <div className="hidden md:flex flex-1 mx-6">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-full border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
+      <div className="hidden md:flex flex-1 justify-center mx-6">
+  <input
+    type="text"
+    placeholder="Search..."
+    className="w-[30%] lg:w-[60%] border border-gray-300 rounded-3xl px-4 py-3 text-lg focus:outline-none focus:ring-2 focus:ring-yellow-500"
+  />
+</div>
+
 
       {/* Right: Auth Buttons */}
-      <div className="flex items-center space-x-4">
-        <button className="text-gray-700 hover:text-blue-600 font-medium">
+      {!isLoggedIn ? <div className="flex items-center space-x-4">
+        <button className="text-gray-700 hover:text-yellow-800 font-medium cursor-pointer">
           Login
         </button>
-        <button className="bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-blue-700 transition">
+        <button className="bg-black text-white px-4 py-2 rounded-full hover:bg-yellow-800 transition cursor-pointer">
           Register
         </button>
-      </div>
+      </div> 
+      :
+    <FaRegUserCircle size={40} className="cursor-pointer text-yellow-300" />
+      }
+      
     </nav>
-   
+   <GameSections/>
     </>
   )
 }
