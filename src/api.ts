fee7cf583b2c778b8c28f7bdc9c756f575d0fc31
Eyad <eyadswailam12345@gmail.com>
2025-10-28 20:31:00 +0300

    add all inital calls in one file

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios"
+const API_BASE = import.meta.env.VITE_APP_API_URL;
+const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+export const getHighestRated = async () => {
+  const res = await axios.get(
+    `${API_BASE}/games?key=${API_KEY}&ordering=-metacritic&page_size=10`
+  );
+  return res.data.results
+    .filter((game: any) => game.background_image)
+    .map((game: any) => ({
+      name: game.name,
+      image: game.background_image,
+    }));
+};
+
+// Get newly released games
+export const getNewlyReleased = async () => {
+  const res = await axios.get(
+    `${API_BASE}/games?key=${API_KEY}&dates=2024-01-01,2025-12-31&ordering=-released&page_size=10`
+  );
+  return res.data.results
+    .filter((game: any) => game.background_image)
+    .map((game: any) => ({
+      name: game.name,
+      image: game.background_image,
+    }));
+};
