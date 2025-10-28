5e0a402accad5efe78559bbb4672ac765d8d83b5
Eyad <eyadswailam12345@gmail.com>
2025-10-28 21:05:13 +0300

    create a hook for lazyloading logic

---
+++ src/hooks/useLazyLoading.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useLazyLoading() {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.5 }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return { ref, isVisible };
+}
