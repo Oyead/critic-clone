1b412d78cd1e601577c92ba5c06078c498116560
Eyad <eyadswailam12345@gmail.com>
2025-11-02 15:59:41 +0200

    add avatar value in authslice

--- src/features/authSlice.ts
+++ src/features/authSlice.ts
@@ -6,7 +6,9 @@ const userData = storedUser ? JSON.parse(storedUser) : null;
 
 const initialState = {
   isLoggedIn: storedLogin === "true",
-  username: userData ? userData.username : "",
+  username: userData?.username || "",
+  email: userData?.email || "",
+  avatar: userData?.avatar || "",
 };
 
 const authSlice = createSlice({
@@ -16,17 +18,39 @@ const authSlice = createSlice({
     login(state, action) {
       state.isLoggedIn = true;
       state.username = action.payload.username;
+      state.email = action.payload.email;
+      state.avatar = action.payload.avatar || "";
+
+      const user = {
+        username: state.username,
+        email: state.email,
+        avatar: state.avatar,
+      };
       localStorage.setItem("isLoggedIn", "true");
-      localStorage.setItem("username", action.payload.username);
+      localStorage.setItem("user", JSON.stringify(user));
     },
     logout(state) {
       state.isLoggedIn = false;
       state.username = "";
+      state.email = "";
+      state.avatar = "";
       localStorage.setItem("isLoggedIn", "false");
-      localStorage.removeItem("username");
+      localStorage.removeItem("user");
+    },
+    updateProfile(state, action) {
+      state.username = action.payload.username;
+      state.email = action.payload.email;
+      state.avatar = action.payload.avatar;
+
+      const user = {
+        username: state.username,
+        email: state.email,
+        avatar: state.avatar,
+      };
+      localStorage.setItem("user", JSON.stringify(user));
     },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, updateProfile } = authSlice.actions;
 export default authSlice.reducer;
