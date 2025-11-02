f3e01aac54f44a767dfbf5b8970ddaf4af450b72
Eyad <eyadswailam12345@gmail.com>
2025-11-02 18:25:02 +0200

    add routing to game details by id

--- src/components/gameCard.tsx
+++ src/components/gameCard.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { useLazyLoading } from "../hooks/useLazyLoading";
+import { Link } from "react-router-dom";
 interface Game {
   name: string;
   image: string;
+  id:number;
 }
 
 const GameCard: React.FC<{ game: Game }> = ({ game }) => {
   const { ref, isVisible } = useLazyLoading();
 
   return (
+  <Link to={`/game/${game.id}`}>
     <div
       ref={ref}
-      className="group shrink-0 w-[320px] h-[200px] rounded-2xl overflow-hidden shadow-lg bg-gray-200 relative"
+      className="group relative shrink-0 w-[320px] h-[200px] rounded-2xl overflow-hidden shadow-lg bg-gray-200 bg-cover bg-center"
+      style={{ backgroundImage: `url(${game.image})` }}
     >
       {isVisible ? (
         <>
           <img
             src={game.image}
             alt={game.name}
-            className="w-full h-full object-cover object-top transition-transform duration-300 group-hover:scale-105 group-hover:brightness-110 cursor-pointer"
+            className="absolute inset-0 w-full h-full object-cover object-center transition-transform duration-300 group-hover:scale-105 group-hover:brightness-110 cursor-pointer"
           />
           {/* overlay on parent, toggled by group-hover */}
           <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-60 transition-opacity duration-300 pointer-events-none" />
@@ -30,6 +34,7 @@ const GameCard: React.FC<{ game: Game }> = ({ game }) => {
         {game.name}
       </p>
     </div>
+    </Link>
   );
 };
 
