ec08026ac007a6e6d0dbda1ca2749dfd29e81720
Eyad <eyadswailam12345@gmail.com>
2025-11-02 21:01:09 +0200

    add review slice to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-
+import reviewsReducer from "../features/reviewSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    reviews:reviewsReducer
   },
 });
 
